bd46df5bed8b6fc2c65e5eda996228ced307cad6
Montana Nicholas <148532409+MontanaNicholas@users.noreply.github.com>
2026-01-22 16:05:00 +0000

    small changes

---
+++ scrapbook/game.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_help_pressed() -> void:
+	pass # Replace with function body.
