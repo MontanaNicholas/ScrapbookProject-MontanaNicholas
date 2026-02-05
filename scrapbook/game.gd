33cc514fbf8b0555b02158645feaa8ca58a8e02f
Montana Nicholas <148532409+MontanaNicholas@users.noreply.github.com>
2026-02-05 14:58:33 +0000

    small changes and assets added

--- scrapbook/game.gd
+++ scrapbook/game.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
-
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 
 func _on_help_pressed() -> void:
-	get_tree().change_scene_to_file("res://help_birthday.tscn")
+	get_tree().change_scene_to_file("res://help_game.tscn")
 
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://BirthdayMinigame.tscn")
+
+
+func _on_blue_pressed() -> void:
+	get_tree().change_scene_to_file("res://food_drink_main.tscn")
