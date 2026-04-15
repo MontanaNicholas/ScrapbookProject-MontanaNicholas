0beb0f4dd05970c9f5e541d38cef3f685320899b
Montana Nicholas <montana171204@gmail.com>
2026-04-16 00:48:58 +0100

    progress and assets to food and drink page

--- scrapbook/food_drink_main.gd
+++ scrapbook/food_drink_main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+@onready var sfx_worm: AudioStreamPlayer = $SFX_WORM
 
+
+
+	
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
-
-
 
 func _on_red_pressed() -> void:
 	get_tree().change_scene_to_file("res://family_friends.tscn")
@@ -15,6 +17,9 @@ func _on_yellow_pressed() -> void:
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://food_drink_minigame.tscn")
+
+func _on_worm_pressed() -> void:
+	Sound.play_worm()
+	get_tree().change_scene_to_file("res://main_menu_worm.tscn")
